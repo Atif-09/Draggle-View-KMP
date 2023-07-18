5a56c121dd208e8b7b0ad457709fc04829bf59bc
Atif-09 <ma2669637@gmail.com>
2023-07-18 22:53:58 +0500

    Added Panning, zooming, rotating

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,5 +1,7 @@
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.unit.IntOffset
@@ -27,13 +30,23 @@ import kotlin.math.roundToInt
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
-    DraggableText("Kotlin Multi Platform")
+    TransformableText("Kotlin Multi Platform")
+//    DraggableText("Kotlin Multi Platform")
     var buttonOffsetX by remember { mutableStateOf(0f) }
     var buttonOffsetY by remember { mutableStateOf(0f) }
     var imageOffsetX by remember { mutableStateOf(0f) }
     var imageOffsetY by remember { mutableStateOf(0f) }
     var greetingText by remember { mutableStateOf("Hello, World!") }
     var showImage by remember { mutableStateOf(false) }
+    // set up all transformation states
+    var scale by remember { mutableStateOf(1f) }
+    var rotation by remember { mutableStateOf(0f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
+    val state = rememberTransformableState { zoomChange, offsetChange, rotationChange ->
+        scale *= zoomChange
+        rotation += rotationChange
+        offset += offsetChange
+    }
 
     Column(
         Modifier.fillMaxSize(),
@@ -45,6 +58,18 @@ fun App() {
                 showImage = !showImage
             },
             modifier = Modifier
+                // apply other transformations like rotation and zoom
+                // on the pizza slice emoji
+                .graphicsLayer(
+                    scaleX = scale,
+                    scaleY = scale,
+                    rotationZ = rotation,
+                    translationX = offset.x,
+                    translationY = offset.y
+                )
+                // add transformable to listen to multitouch transformation events
+                // after offset
+                .transformable(state = state)
                 .offset { IntOffset(buttonOffsetX.roundToInt(), buttonOffsetY.roundToInt()) }
                 .pointerInput(Unit) {
                     detectDragGestures { change, dragAmount ->
@@ -60,6 +85,18 @@ fun App() {
         if (showImage) {
             Box(
                 Modifier
+                    // apply other transformations like rotation and zoom
+                    // on the pizza slice emoji
+                    .graphicsLayer(
+                        scaleX = scale,
+                        scaleY = scale,
+                        rotationZ = rotation,
+                        translationX = offset.x,
+                        translationY = offset.y
+                    )
+                    // add transformable to listen to multitouch transformation events
+                    // after offset
+                    .transformable(state = state)
                     .offset { IntOffset(imageOffsetX.roundToInt(), imageOffsetY.roundToInt()) }
                     .pointerInput(Unit) {
                         detectDragGestures { change, dragAmount ->
@@ -79,14 +116,34 @@ fun App() {
 }
 
 
-
-@Composable
+/*@Composable
 private fun DraggableText(text: String) {
     var offsetX by remember { mutableStateOf(0f) }
     var offsetY by remember { mutableStateOf(0f) }
+    // set up all transformation states
+    var scale by remember { mutableStateOf(1f) }
+    var rotation by remember { mutableStateOf(0f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
+    val state = rememberTransformableState { zoomChange, offsetChange, rotationChange ->
+        scale *= zoomChange
+        rotation += rotationChange
+        offset += offsetChange
+    }
 
     Text(
         modifier = Modifier
+            // apply other transformations like rotation and zoom
+            // on the pizza slice emoji
+            .graphicsLayer(
+                scaleX = scale,
+                scaleY = scale,
+                rotationZ = rotation,
+                translationX = offset.x,
+                translationY = offset.y
+            )
+            // add transformable to listen to multitouch transformation events
+            // after offset
+            .transformable(state = state)
             .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
             .wrapContentHeight()
             .wrapContentWidth()
@@ -100,27 +157,65 @@ private fun DraggableText(text: String) {
         text = text,
         color = Color.Black
     )
-}
-
-/*fun App() {
-    MaterialTheme {
-        var greetingText by remember { mutableStateOf("Hello, World!") }
-        var showImage by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = {
-                greetingText = "Hello, ${getPlatformName()}"
-                showImage = !showImage
-            }) {
-                Text(greetingText)
-            }
-            AnimatedVisibility(showImage) {
-                Image(
-                    painterResource("compose-multiplatform.xml"),
-                    null
-                )
-            }
-        }
-    }
 }*/
+
+@Composable
+private fun TransformableText(text: String) {
+    var offsetX by remember { mutableStateOf(0f) }
+    var offsetY by remember { mutableStateOf(0f) }
+    // set up all transformation states
+    var scale by remember { mutableStateOf(1f) }
+    var rotation by remember { mutableStateOf(0f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
+    val state = rememberTransformableState { zoomChange, offsetChange, rotationChange ->
+        scale *= zoomChange
+        rotation += rotationChange
+        offset += offsetChange
+    }
+    Box(
+        Modifier
+            // apply other transformations like rotation and zoom
+            // on the pizza slice emoji
+            .graphicsLayer(
+                scaleX = scale,
+                scaleY = scale,
+                rotationZ = rotation,
+                translationX = offset.x,
+                translationY = offset.y
+            )
+            // add transformable to listen to multitouch transformation events
+            // after offset
+            .transformable(state = state)
+            .fillMaxSize()
+    ) {
+        Text(
+            modifier = Modifier
+                // apply other transformations like rotation and zoom
+                // on the pizza slice emoji
+                .graphicsLayer(
+                    scaleX = scale,
+                    scaleY = scale,
+                    rotationZ = rotation,
+                    translationX = offset.x,
+                    translationY = offset.y
+                )
+                // add transformable to listen to multitouch transformation events
+                // after offset
+                .transformable(state = state)
+                .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+                .wrapContentHeight()
+                .wrapContentWidth()
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+                        offsetX += dragAmount.x
+                        offsetY += dragAmount.y
+                    }
+                },
+            text = text,
+            color = Color.Black
+        )
+    }
+}
 
 expect fun getPlatformName(): String
